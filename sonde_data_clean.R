90243b7b9cc2fc597d4152ae88f9e53a65278267
elisemorrison <elisesmorrison@gmail.com>
2022-12-16 22:43:09 -0500

    update plots with dates highlighted

--- sonde_data_clean.R
+++ sonde_data_clean.R
@@ -82,6 +82,12 @@ agglomerateddata<-read.csv("SondeOutput/2022_12_12_sonde_data_agglomerated.csv")
 #date does not import correctly
 agglomerateddata$Date_ymd<-as_date(agglomerateddata$Date_ymd)
 
+#Piney Point Sonde was called CockRoach Bay, other sites don't have space in names, correct names
+agglomerateddata$Site_ID<-gsub("CockroachBay", "Piney Point", agglomerateddata$Site_ID)
+agglomerateddata$Site_ID<-gsub("BishopHarbor", "Bishop Harbor", agglomerateddata$Site_ID)
+agglomerateddata$Site_ID<-gsub("JoeBay", "Joe Bay", agglomerateddata$Site_ID)
+agglomerateddata$Site_ID<-gsub("StJosephSound", "St. Joseph Sound", agglomerateddata$Site_ID)
+
 
 #BH has anomalous spike in Oct 2021
 #JB very low in March 2022
@@ -94,38 +100,23 @@ trimmed<-agglomerateddata %>%
          `Chlorophyll ug/L` = replace(`Chlorophyll ug/L`, `Chlorophyll ug/L`>400 | `Chlorophyll ug/L`<0, NA),
          `fDOM RFU` = replace(`fDOM RFU`, `fDOM RFU`<0 | `fDOM RFU` >100, NA), 
          `ODO % sat` = replace(`ODO % sat`, `ODO % sat`<0  | `ODO % sat`>500, NA),
-         `ODO mg/L` = replace(`ODO mg/L`, `ODO mg/L`<0  | `ODO mg/L`>50, NA),
+         `ODO mg/L` = replace(`ODO mg/L`, `ODO mg/L`<0  | `ODO mg/L`>20, NA), #a few anomalously high values in SSJS
          `SpCond µS/cm` = replace(`SpCond µS/cm`, `SpCond µS/cm`<0  | `SpCond µS/cm`>200, NA),                      
-         `BGA PE RFU` = replace(`BGA PE RFU`, `BGA PE RFU`<0  | `BGA PE RFU`>100, NA),
+         `BGA PE RFU` = replace(`BGA PE RFU`, `BGA PE RFU`<0  | `BGA PE RFU`>40, NA), #had anomalously high values in Feb/March, likely issue with sensor
          `BGA PE ug/L` = replace(`BGA PE ug/L`, `BGA PE ug/L`<0  | `BGA PE ug/L`>280, NA),                   
          `Turbidity FNU` = replace(`Turbidity FNU`, `Turbidity FNU`<0  | `Turbidity FNU`>4000, NA), 
          `Temp °C` = replace(`Temp °C`, `Temp °C`<5  | `Temp °C`>50, NA),
          `pH` = replace(`pH`, `pH`<0  | `pH`>14, NA),
+         `Sal psu` = replace(`Sal psu`, `pH`<0  | `Sal psu`>35, NA),
          `ORP mV` = replace(`ORP mV`, `ORP mV`<-999  | `ORP mV`>999, NA))
-
-
-########### TO DO: repeat this approach with all sensors
-
-fDOM<-ggplot(agglomerateddata, aes(`Date_ymd`, `fDOM RFU`))+
-  geom_point()+
-  geom_smooth(se=FALSE)+
-  facet_wrap(.~Site_ID, scales="free_y")+
-  theme_cowplot(font_size = 8)+
-  panel_border(color="black")+
-  geom_vline(xintercept = as.Date("2021-07-05"), linetype=4, color="black")+
-  geom_vline(xintercept = as.Date("2021-04-07"), linetype=4, color="red")+
-  theme(text = element_text(size = 14), 
-        axis.text = element_text(size = 9), 
-        axis.text.x = element_text(angle = -45, hjust = 0.1, vjust = 0.1)
-  ) 
-
 
 #### generate summary statistics for all columns
 summary_all_data<-trimmed %>% 
   group_by(Site_ID, Date_ymd) %>% 
-  summarise_all(list(min=min, max=max, mean=mean, sd=sd), na.rm=TRUE)
+  summarise_all(list(mean=mean, sd=sd, min=min, max=max), na.rm=TRUE)
 
 write.csv(summary_all_data, "SondeOutput/2022_12_14_sonde_data_agglomerated_summarized.csv")
+
 
 #generate test plots
 fDOM<-ggplot(summary_all_data, aes(`Date_ymd`, `fDOM RFU_mean`))+
@@ -141,39 +132,67 @@ fDOM<-ggplot(summary_all_data, aes(`Date_ymd`, `fDOM RFU_mean`))+
         axis.text.x = element_text(angle = -45, hjust = 0.1, vjust = 0.1)
   ) 
 
+sal<-ggplot(summary_all_data, aes(`Date_ymd`, `Sal psu_mean`))+
+  geom_rect(aes(ymin=-Inf, ymax=Inf, xmin=as.Date("2021-03-30"), xmax=as.Date("2021-04-09")), fill="#00BA38", alpha=0.002)+
+  geom_rect(aes(ymin=-Inf, ymax=Inf, xmin=as.Date("2021-06-13"), xmax=as.Date("2021-07-18")), fill="red", alpha=0.002)+
+  geom_point()+
+  geom_smooth(se=FALSE)+
+  facet_wrap(.~Site_ID, scales="free_y")+
+  theme_cowplot(font_size = 8)+
+  panel_border(color="black")+
+  geom_vline(xintercept = as.Date("2021-07-05"), linetype=4, color="black")+
+  theme(text = element_text(size = 12), 
+        axis.text = element_text(size = 9), 
+        axis.text.x = element_text(angle = -45, hjust = 0.1, vjust = 0.1))+
+  ylab("Salinity (psu)")+
+  xlab("")
 
 
 ODO_mg_L<-ggplot(summary_all_data, aes(`Date_ymd`, `ODO mg/L_mean`))+
+  geom_rect(aes(ymin=-Inf, ymax=Inf, xmin=as.Date("2021-03-30"), xmax=as.Date("2021-04-09")), fill="#00BA38", alpha=0.002)+
+  geom_rect(aes(ymin=-Inf, ymax=Inf, xmin=as.Date("2021-06-13"), xmax=as.Date("2021-07-18")), fill="red", alpha=0.002)+
   geom_point()+
   geom_smooth(se=FALSE)+
   facet_wrap(.~Site_ID, scales="free_y")+
   theme_cowplot(font_size = 8)+
   panel_border(color="black")+
   geom_vline(xintercept = as.Date("2021-07-05"), linetype=4, color="black")+
-  geom_vline(xintercept = as.Date("2021-04-07"), linetype=4, color="red")+
-  theme(text = element_text(size = 14), 
+  theme(text = element_text(size = 12), 
         axis.text = element_text(size = 9), 
-        axis.text.x = element_text(angle = -45, hjust = 0.1, vjust = 0.1)
-  ) 
+        axis.text.x = element_text(angle = -45, hjust = 0.1, vjust = 0.1))+
+  ylab("Dissolved oxygen (mg/L)")+
+  xlab("")
 
-chl_RFU<-ggplot(summary_all_data, aes(`Date_ymd`, `Chlorophyll RFU_mean`))+
+chl_mgL<-ggplot(summary_all_data, aes(`Date_ymd`, `Chlorophyll ug/L_mean`))+
+  geom_rect(aes(ymin=-Inf, ymax=Inf, xmin=as.Date("2021-03-30"), xmax=as.Date("2021-04-09")), fill="#00BA38", alpha=0.002)+
+  geom_rect(aes(ymin=-Inf, ymax=Inf, xmin=as.Date("2021-06-13"), xmax=as.Date("2021-07-18")), fill="red", alpha=0.002)+
   geom_point()+
   geom_smooth(se=FALSE)+
   facet_wrap(.~Site_ID, scales="free_y")+
   theme_cowplot(font_size = 8)+
   geom_vline(xintercept = as.Date("2021-07-05"), linetype=4, color="black")+
-  geom_vline(xintercept = as.Date("2021-04-07"), linetype=4, color="red")+
-  panel_border(color="black")
+  panel_border(color="black")+
+  theme(text = element_text(size = 12), 
+        axis.text = element_text(size = 9), 
+        axis.text.x = element_text(angle = -45, hjust = 0.1, vjust = 0.1))+
+  ylab("In situ chlorophyll (ug/L)")+
+  xlab("")
 
 PE_RFU<-ggplot(summary_all_data, aes(`Date_ymd`, `BGA PE RFU_mean`))+
+  geom_rect(aes(ymin=-Inf, ymax=Inf, xmin=as.Date("2021-03-30"), xmax=as.Date("2021-04-09")), fill="#00BA38", alpha=0.002)+
+  geom_rect(aes(ymin=-Inf, ymax=Inf, xmin=as.Date("2021-06-13"), xmax=as.Date("2021-07-18")), fill="red", alpha=0.002)+
   geom_point()+
   geom_smooth(se=FALSE)+
   facet_wrap(.~Site_ID, scales="free_y")+
   theme_cowplot(font_size = 8)+
   panel_border(color="black")+
   geom_vline(xintercept = as.Date("2021-07-05"), linetype=4, color="black")+
-  geom_vline(xintercept = as.Date("2021-04-07"), linetype=4, color="red")+
-  theme(text = element_text(size = 14), 
+  theme(text = element_text(size = 12), 
         axis.text = element_text(size = 9), 
-        axis.text.x = element_text(angle = -45, hjust = 0.1, vjust = 0.1)
-  ) 
+        axis.text.x = element_text(angle = -45, hjust = 0.1, vjust = 0.1))+
+  ylab("Phycoerythrin (RFU)")+
+  xlab("")
+
+sonde_plot<-plot_grid(sal, ODO_mg_L, chl_mgL, PE_RFU, labels = c("A", "B", "C", "D"))
+
+ggsave("./Figures/2022_12_16_sonde_plot.png", sonde_plot)
